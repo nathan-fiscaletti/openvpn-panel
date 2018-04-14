8d1adcd15399ace4f4467b896841ae30621612f4
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-04-14 12:05:52 -0500

    Developing

--- content/pages/ListClients.php
+++ content/pages/ListClients.php
@@ -9,6 +9,9 @@
 <div class="container">
         <div class="card card-container">
             <div align='center'><h3>Clients</h3><br /></div>
+            <div class="well" align='center'>
+                Click on a client to open it' management panel.
+            </div>
             <?php foreach ($clients as $client) { ?>
                 <a href='?section=manage_client&client=<?php echo $client; ?>' style='text-decoration: none;'>
                     <button class="btn btn-lg btn-primary btn-block btn-signin">
