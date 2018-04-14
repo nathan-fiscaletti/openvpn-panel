0e6008484f69beef37de7f7d5fe44aaf764b3e10
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-04-14 17:48:14 -0500

    Updated ServerLog.php

--- content/pages/ServerLog.php
+++ content/pages/ServerLog.php
@@ -1,5 +1,5 @@
 <div class="container">
         <div class="well" align='center'>
-            <pre align='left'><?php echo file_get_contents('/etc/openvpn/server.log'); ?></pre>
+            <pre align='left' style='max-height: 100%;'><?php echo file_get_contents('/etc/openvpn/server.log'); ?></pre>
         </div>
 </div><!-- /container -->
