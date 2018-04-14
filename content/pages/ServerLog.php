80fda0b9aecdc55345c5327c0d1b5485c946b33e
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-04-14 12:42:43 -0500

    Developing

--- content/pages/ServerLog.php
+++ content/pages/ServerLog.php
@@ -1,6 +1,6 @@
 <div class="container">
         <div class="well" align='center'>
-            <pre>
+            <pre align='left'>
                 <?php echo file_get_contents('/etc/openvpn/server.log'); ?>
             </pre>
         </div>
