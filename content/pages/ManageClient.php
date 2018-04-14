c14eae8f5907d9f168feba9414edf301d3ed5dab
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-04-14 13:38:11 -0500

    Developing

--- content/pages/ManageClient.php
+++ content/pages/ManageClient.php
@@ -20,8 +20,8 @@
 <div class="container">
         <div class="card card-container">
             <div align='center'><h3><?php echo $_GET['client']; ?></h3><br /></div>
-            <a href='download_client.php?client=<?php echo $_GET['client']; ?>' style='text-decoration: none;margin-bottom: 5px;'>
-                <button class="btn btn-lg btn-primary btn-block btn-signin">Download Client Configuration</button>
+            <a href='download_client.php?client=<?php echo $_GET['client']; ?>' style='text-decoration: none;'>
+                <button class="btn btn-lg btn-primary btn-block btn-signin" style='margin-bottom: 5px;'>Download Client Configuration</button>
             </a>
             <button class="btn btn-lg btn-primary btn-block btn-signin">Manage Routes For Client</button>
             <button class="btn btn-lg btn-primary btn-block btn-signin">Delete Client</button>
