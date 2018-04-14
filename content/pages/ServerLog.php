5ef7f6b67683e6ba8400dc6bba77e582838df915
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-04-14 17:50:51 -0500

    Updated ServerLog.php

--- content/pages/ServerLog.php
+++ content/pages/ServerLog.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container" align='center'>
         <h2>Server Log</h2>
         <div class="well" align='center'>
             <pre align='left' style='max-height: 600px;'><?php echo file_get_contents('/etc/openvpn/server.log'); ?></pre>
