845ec55f1613e495d69b3122c0514b048c622759
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-04-14 10:23:15 -0500

    Developing

--- content/pages/ListClients.php
+++ content/pages/ListClients.php
@@ -8,7 +8,7 @@
 
 <div class="container">
         <div class="card card-container">
-            <div align='center'><h3>Clients</h3></div>
+            <div align='center'><h3>Clients</h3><br /></div>
             <?php foreach ($clients as $client) { ?>
                 <button class="btn btn-lg btn-primary btn-block btn-signin"><?php echo $client; ?></button>
             <?php } ?>
